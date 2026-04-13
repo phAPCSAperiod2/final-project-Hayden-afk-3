bfe2367ddd8dcbd4f8eab8db4f42fb81f568ef7f
Hayden-afk-3 <hbdaugherty09@gmail.com>
2026-04-13 17:37:56 +0000

    Set up List skeletal structure and setDate

---
+++ src/List.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class List {
+    private ArrayList<Assignment> assignments;
+
+    public List() {
+        assignments = new ArrayList<>();
+    }
+
+    public void addAssignment(Assignment assignment) {
+        int insertIndex = assignments.size();
+
+        for (int i = 0; i < assignments.size(); i++) {
+            Assignment current = assignments.get(i);
+
+            if (assignment.getDueDate() < current.getDueDate()) {
+                insertIndex = i;
+                break;
+            } else if (assignment.getDueDate() == current.getDueDate()) {
+                if (assignment.getPointsWorth() > current.getPointsWorth()) {
+                    insertIndex = i;
+                    break;
+                }
+            }
+        }
+
+        assignments.add(insertIndex, assignment);
+    }
+
+    public ArrayList<Assignment> getAssignments() {
+        return assignments;
+    }
+
+    public Assignment getAssignment(int index) {
+        return assignments.get(index);
+    }
+
+    public int size() {
+        return assignments.size();
+    }
+}
